325b23ea62fa6e453548c54691e001cc778ca787
Robin <r.s.mar@gmx.de>
2024-01-22 13:14:21 +0100

    add wikipedia dicts

--- project/main.py
+++ project/main.py
@@ -1,27 +1,30 @@
 from objects.receipe import Receipe
-from modules.label_extractor import LabelExtractor 
+from modules.label_extractor import LabelExtractor
 
-def run_pipeline(receipe: Receipe) -> Receipe:   
+
+def run_pipeline(receipe: Receipe) -> Receipe:
     # Add Modules here:
-    
+
     # TODO: Origin Extractor
-    
+
     # TODO: Wiki Api Call
-    
+
     # TODD: Translate to english
-    
+
     # TODO: Label Extractor
     label_extractor = LabelExtractor(receipe=receipe)
     updated_receipe = label_extractor.run()
     # TODO: Label Finalizer
-    
+
     return updated_receipe
+
 
 def print_receipe(receipe: Receipe):
     receipe.print()
 
+
 if __name__ == '__main__':
-    receipe = Receipe("Spicy Garlic Shrimp Pasta", 
+    receipe = Receipe("Spicy Garlic Shrimp Pasta Italian",
                       """
                         This Spicy Garlic Shrimp Pasta is a delightful fusion of bold flavors 
                         and comforting textures. Succulent shrimp are sautéed to perfection
@@ -34,10 +37,10 @@ if __name__ == '__main__':
                         your cravings for a flavorful and satisfying meal.")
                     
                     """,
-                    "",
-                    [])
-    
+                      "",
+                      "",
+                      [])
+
     updated_receipe = run_pipeline(receipe=receipe)
-    
+
     print(updated_receipe)
-    
