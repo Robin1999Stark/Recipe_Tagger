07b0f4730264d733e1098cd69a93a6eae9ed2764
Robin <r.s.mar@gmx.de>
2024-01-24 01:25:36 +0100

    add note

--- project/main.py
+++ project/main.py
@@ -4,6 +4,10 @@ from example_data import recipes
 from recipe_tagger import RecipeTagger
 
 # Start program from this file
+# Before running this program for the first time call:
+# pipenv run python -m spacy download en_core_web_sm
+# to download the spacy english dataset
+
 if __name__ == '__main__':
 
     dl = DatasetLoader()
