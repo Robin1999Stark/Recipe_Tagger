49e64e967a41605205ef64d77b583f609e4de3f7
Robin1999Stark <r.s.mar@gmx.de>
2024-01-25 14:38:14 +0100

    add datasets

--- project/test.py
+++ project/test.py
@@ -1,6 +1,6 @@
 from objects.receipe import print_receipe
 from modules.datasetloader import DatasetLoader
-from example_data import recipes
+from example_data import recipes_label_extractor
 from modules.label_extractor import LabelExtractor
 # Start program from this file
 # Before running this program for the first time call:
@@ -14,6 +14,6 @@ if __name__ == '__main__':
 
     extractor = LabelExtractor(nlp=nlp)
 
-    for recipe in recipes:
+    for recipe in recipes_label_extractor:
         updated_receipe = extractor.run(recipe=recipe)
         print_receipe(updated_receipe)
